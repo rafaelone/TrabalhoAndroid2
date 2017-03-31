3eeb54fc0299a22256f6853c7c65f8c047d2bc2d
Casa <casa@localhost.localdomain>
2017-03-31 18:18:07 -0300

    ativacao da splash screen

--- app/src/main/java/br/com/fiap/exercicios/listview/RM77722/PreferenciasActivity.java
+++ app/src/main/java/br/com/fiap/exercicios/listview/RM77722/PreferenciasActivity.java
@@ -5,13 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class PreferenciasActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferencias);
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
