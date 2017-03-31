3903db5556d3ba9d9891f0df8b7b8d8ef53db653
logonrm <logonrm@fiap.com.br>
2017-03-31 11:39:29 -0300

    pizza de calabresa feita

--- app/src/main/java/br/com/fiap/exercicios/listview/RM77722/GerarPizza.java
+++ app/src/main/java/br/com/fiap/exercicios/listview/RM77722/GerarPizza.java
@@ -24,7 +24,28 @@ public class GerarPizza {
                 "Tomate em rodelas a gosto\n" +
                 "Orégano a gosto",
                 "Se você tem alergia a alguns produtos derivados de leite, por favor não coma essa pizza", "5" ));
-        pizzas.add(new Pizza("Aquela pizza de calabresa ", R.drawable.calabresa, "R$ 11,00", "Pizza de Calabresa","ingredientes", "alergicos", "5" ));
+
+        pizzas.add(new Pizza("Aquela pizza de calabresa ", R.drawable.calabresa, "R$ 11,00", "Pizza de Calabresa","Massa: \n" +
+                "500g de farinha de trigo\n" +
+                "25 g de fermento para pão\n" +
+                "1 copo de água\n" +
+                "1 ovo\n" +
+                "50 g de gordura\n" +
+                "1 colher(sopa) fermento em pó \n" +
+                "1 colher(café) de sal\n" +
+                "Molho\n" +
+                "300g tomate maduro\n" +
+                "50g queijo parmesão ralado\n" +
+                "Sal\n" +
+                "Orégano\n" +
+                "1 colher(sopa) azeite\n" +
+                "Cobertura\n" +
+                "300 g de lingui calabresa\n" +
+                "2 cebolas em rodelas finas\n" +
+                "1/2 xícara(chá) azeitona preta\n" +
+                "1 colher(sopa) de azeite\n" +
+                "1 colher(café) de orégano",
+                "Se você tem alergia a alguns produtos derivados de leite, por favor não coma essa pizza", "5" ));
         pizzas.add(new Pizza("Curte pizza de catupiry ?", R.drawable.catupiry, "R$ 12,00", "Pizza de Catupiry","ingredientes", "alergicos", "5"));
         pizzas.add(new Pizza("Vai uma portuguesa ?", R.drawable.portuguesa, "R$ 13,00", "Pizza de portuguesa","ingredientes", "alergicos", "5" ));
         pizzas.add(new Pizza("Uhm... e a de chocolate ?", R.drawable.chocolate, "R$ 14,00", "Pizza de Chocolate","ingredientes", "alergicos", "5" ));
