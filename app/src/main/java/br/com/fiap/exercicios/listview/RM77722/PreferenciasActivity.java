00ef96ece1ba52d7111e9a34ca5274a259937a07
Casa <casa@localhost.localdomain>
2017-04-02 10:46:00 -0300

    finalizado

--- app/src/main/java/br/com/fiap/exercicios/listview/RM77722/PreferenciasActivity.java
+++ app/src/main/java/br/com/fiap/exercicios/listview/RM77722/PreferenciasActivity.java
@@ -1,34 +1,59 @@
 package br.com.fiap.exercicios.listview.RM77722;
 
+
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RadioButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
 
 public class PreferenciasActivity extends AppCompatActivity {
 
-
+    SharedPreferences sp;
+    EditText splashScreen;
+    Switch desabilidarSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferencias);
 
+        splashScreen = (EditText) findViewById(R.id.splashScreenField);
+        desabilidarSplash = (Switch) findViewById(R.id.disableSplash);
+        sp = getSharedPreferences("PizzariaPreferences", MODE_PRIVATE);
+
+
+        splashScreen.setText(String.valueOf(sp.getInt("splashTime", 3000)));
+        desabilidarSplash.setChecked(sp.getBoolean("disableSplash", false));
 
         ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getString(R.string.preferences));
         actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
+    public void savePreferences(View view) {
+        int time = Integer.valueOf( splashScreen.getText().toString() );
+        boolean disable = desabilidarSplash.isChecked();
+
+        SharedPreferences.Editor e = sp.edit();
+        e.putBoolean("disableSplash", disable);
+        e.putInt("splashTime", time);
+        e.commit();
+        finish();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            finish();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void salvar(View view) {
-
     }
 }
--- app/src/main/java/br/com/fiap/exercicios/listview/RM77722/SplashScreenActivity.java
+++ app/src/main/java/br/com/fiap/exercicios/listview/RM77722/SplashScreenActivity.java
@@ -1,29 +1,44 @@
 package br.com.fiap.exercicios.listview.RM77722;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent it = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(it);
+        sp = getSharedPreferences("PizzariaPreferences", MODE_PRIVATE);
 
-                finish();
+        int splashTime = sp.getInt("splashTime", 3000);
+        boolean disable = sp.getBoolean("disableSplash", false);
 
-            }
-        }, 2000);
+        if( !disable ) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    loadMainActivity();
+                }
+            }, splashTime);
+        } else {
+            loadMainActivity();
+        }
+    }
+
+    public void loadMainActivity() {
+        Intent it = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(it);
+
+        finish();
+    }
     }
 
 
-}
+
